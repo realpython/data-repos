bb8fffca27b60360c5bd40e6f48ea0e05fcdc2c0
Ian <iansedano@gmail.com>
2023-02-10 10:51:48 +0100

    adding test docstrings and interrogate ignore paths

--- tests/test_data_repo.py
+++ tests/test_data_repo.py
@@ -1,3 +1,6 @@
+"""Tests for data_repo.read
+"""
+
 # Third party imports
 import pytest
 
@@ -6,13 +9,16 @@ from data_repo import read
 
 
 def test_read_sample_countries_csv_data():
+    """Test reading the sample country CSV file"""
     assert not read.data("countries").empty
 
 
 def test_read_sample_cars_json_data():
+    """Test reading the sample cars JSON file"""
     assert not read.data("cars").empty
 
 
 def test_raise_exception_no_dataset():
+    """Test raising exception when called data file doesn't exist"""
     with pytest.raises(FileNotFoundError):
         read.data("no_data")
